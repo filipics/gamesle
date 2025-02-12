6b09673fdd11cfc957cde7de3484c083299fb411
filipics <filipichjuan@gmail.com>
2025-02-11 22:10:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ document.getElementById('statsButton').addEventListener('click', () => {
 document.getElementById('game1').addEventListener('click', () => {
   window.location.href = '/games/wordle-game/index.html'; // Cambiado a /games
 });
-
+document.getElementById('game2').addEventListener('click', () => {
+  window.location.href = '/games/mundo-game/index.html'; // Cambiado a /games
+});
 // Aquí puedes agregar más lógica para otros juegos de la misma manera
 // Ejemplo para el Juego 2:
 document.querySelectorAll('.game-btn').forEach((button, index) => {
